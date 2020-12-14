a53d78c5472bc0512e24598580ad679f7850b811
ocawthorne <ocawthorne@gmail.com>
2020-12-14 21:29:57 +0000

    Establish controllers

--- db/migrate/20201214211018_create_initial_tables.rb
+++ db/migrate/20201214211018_create_initial_tables.rb
@@ -16,6 +16,7 @@ class CreateInitialTables < ActiveRecord::Migration[6.0]
     create_table :patients do |t|
       t.string :first_name
       t.string :surname
+      t.datetime :date_of_birth
       t.timestamps null: false
     end
     
