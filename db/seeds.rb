c4f41d31505ae7256223d224a625d514b7dc1b26
ocawthorne <ocawthorne@gmail.com>
2020-12-15 15:55:08 +0000

    Enter seed data into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Doctor.create([
+   {
+      first_name: "Adam",
+      surname: "Botticelli"
+   },
+   {
+      first_name: "Charles",
+      surname: "Darwin"
+   },
+   {
+      first_name: "Erwin",
+      surname: "Foxhill"
+   }
+])
+
+Patient.create([
+   {
+      first_name: "Arnold",
+      middle_name: "Bernard",
+      surname: "Clark"
+   },
+   {
+      first_name: "Diane",
+      middle_name: "Eleanor",
+      surname: "Florence"
+   },
+   {
+      first_name: "Georgia",
+      middle_name: "Harriet",
+      surname: "Ipsley"
+   }
+])
+
+Drug.create([
+   {name: "Paracetamol"}, {name: "Warfarin"}, {name: "Aspirin"}, {name: "Phenytoin"}, {name: "Microgynon"}
+   #! Paracetamol interacts with Phenytoin
+   #! Warfarin interacts with Phenytoin
+   #! Warfarin interacts with Aspirin
+   #! Microgynon interacts with Phenytoin
+])
+
+Condition.create([
+   {name: "Epilepsy"}, #! Use Phenytoin
+   {name: "Haemophilia"}, #! Do NOT use Aspirin
+   {name: "Stroke"} #! Use Warfarin
+
+])
