3d06eb14f00f3f8de4204b911a600c94c714fa2e
ocawthorne <ocawthorne@gmail.com>
2021-01-04 21:43:11 +0000

    Fix patient sign up redirection

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -14,7 +14,12 @@ class PatientsController < ApplicationController
          email: params[:email],
          password: params[:password]
       )
-      @patient.errors ? render('new') : (redirect_to patient_path(@patient))
+      if @patient.errors.any?
+         render 'new'
+      else
+         session[:patient_id] = @patient.id
+         redirect_to patient_path(@patient)
+      end
    end
 
    def show
